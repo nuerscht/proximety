30df3e0c501d84b9e1bd8fd89fd5bfd7af77d808
Patrick Bösch <patrick.boesch@vision.ch>
2014-12-18 19:45:20 +0100

    - monitoring was successful :)

---
+++ app/src/main/java/ch/ffhs/esa/proximety/service/binder/ServiceBinder.java
@@ -0,0 +1,42 @@
+package ch.ffhs.esa.proximety.service.binder;
+
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import ch.ffhs.esa.proximety.domain.test.JsonTestValidate;
+import ch.ffhs.esa.proximety.service.client.RestClient;
+import ch.ffhs.esa.proximety.service.handler.ResponseHandler;
+
+/**
+ * Created by boe on 18.12.2014.
+ */
+public class ServiceBinder {
+    public JsonTestValidate get(final ResponseHandler responseHandler) {
+
+        RestClient.get("", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Gson gson = new Gson();
+                responseHandler.onSuccess(statusCode, headers, gson.fromJson(response.toString(), JsonTestValidate.class));
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                Gson gson = new Gson();
+                responseHandler.onSuccess(statusCode, headers, gson.fromJson(response.toString(), JsonTestValidate.class));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                responseHandler.onError(statusCode, headers, throwable, errorResponse);
+            }
+        });
+
+        return null;
+    }
+}
