9ea8604eefe42d4d60df0ef987b5d8ef7d393995
Jonas Alder <jonas.alder@gmail.com>
2014-12-19 19:01:43 +0100

    fixed reference for friend detail menu

--- app/src/main/java/ch/ffhs/esa/proximety/activities/FriendDetailActivity.java
+++ app/src/main/java/ch/ffhs/esa/proximety/activities/FriendDetailActivity.java
@@ -70,29 +70,6 @@ public class FriendDetailActivity extends FragmentActivity implements ActionBar.
                             .setTabListener(this));
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_friend_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
         // When the given tab is selected, switch to the corresponding page in
